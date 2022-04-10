77eef91e6e990820f09988422604b24400ac7d5f
Artem_Vaulin <vaulin1506@gmail.com>
2022-04-10 22:04:33 +0300

    Revert "Junit5"
    This reverts commit e569f05e614e151080230f747e7742d29f9edf97.

--- src/test/java/com/vaulin1506/simple_test.java
+++ src/test/java/com/vaulin1506/simple_test.java
@@ -1,23 +1,4 @@
 package com.vaulin1506;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 public class simple_test {
-    @BeforeEach
-    void openPage() {
-        Selenide.open("https://demoqa.com/automation-practice-form");
-    }
-    @AfterEach
-    void close() {
-        WebDriverRunner.closeWindow();
-    }
-    @Test
-    void assertTest(){
-
-    }
-
 }
